846c741d6c72e3826b7ef3f16be8658ae2c7edfd
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-15 01:24:30 -0500

    initial card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h2>Card Title</h2>
+            <p>Card content goes here.</p>
+        </div>
+    );
+};
+
+export default Card;
