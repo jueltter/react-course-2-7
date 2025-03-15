32e752f0d4cb1ef6f79aae534123f9cd56d4b901
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-15 01:28:16 -0500

    card css module

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './styles.css';
+import styles from './Card.module.css';
 
 const Card = () => {
     return (
-        <div className="card">
+        <div className={`${styles.card}`}>
             <h2>Card Title</h2>
             <p>Card content goes here.</p>
         </div>
